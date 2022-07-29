7eaa54f0921c8a693f78fc5a6f18ee6735e9fc9e
Galo Pocoma <gpocoma@msn.com>
2022-07-29 13:04:01 -0400

    Fix #6

--- frontend/views.py
+++ frontend/views.py
@@ -27,7 +27,8 @@ DIC = {
         DECANO
     ''',
     'lista_epitelio': [
-        'Epitelio plano simple','Epitelio cúbico simple',
+        'Epitelio plano simple',
+        'Epitelio cúbico simple',
         'Epitelio cilíndrico simple',
         'Epitelio cilíndrico simple con esterocilios',
         'Epitelio cilíndrico simple con microvellosidades',
@@ -46,7 +47,192 @@ DIC = {
         'Ácinos serosos',
         'Glándulas mixtas',
         'Glándulas alveolares'
-    ]
+    ],
+    'lista_tejidos': {
+        'Conectivo': [
+            'Fibras de colágeno densas y laxas',
+            'Tejido conectivo denso regular',
+            'Tejido reticular',
+            'Tejido conecto elástico',
+            'Plasmocitos',
+            'Histocitos',
+            'Mesénquima',
+            'Tejido mucoso',
+            'Tejido adiposo blanco',
+            'Tejido adiposo pardo',
+        ],
+        'Cartilaginoso': [
+            'Cartilago hialino',
+            'Cartilago elástico',
+            'Cartilago elástico, Orceína',
+            'Fibrocartílago, Disco Intervertebral',
+            'Fibrocartílago, Menisco'
+        ],
+        'Óseo': [
+            'Hueso desgastado, Corte transversal',
+            'Hueso desgastado, Corte longitudinal',
+            'Hueso compacto descalificado',
+            'Hueso compacto trabecular',
+            'Osteoclastos',
+            'Osificación en base membranosa, Mascarilla fetal',
+            'Osificación endocondral, Mano fetal',
+            'Articulación de codo'
+        ],
+        'Muscular': [
+            'Músculo estriado esquelético. Corte longitudinal',
+            'Músculo estriado esquelético. Corte transversal',
+            'Músculo estriado esquelético. Hematoxilina férrica',
+            'Músculo estriado esquelético. Hematoxilina fosfotúngstica',
+            'Músculo cardiaco. Corte longitudinal',
+            'Músculo cardiaco. Corte transversal',
+            'Músculo cardiaco. Hematoxilina fosfotúngstica',
+            'Músculo liso. Intestino delgado',
+            'Músculo liso. Miometrio'
+        ],
+        'Nervioso': [
+            'Cerebro',
+            'Cerebelo',
+            'Cerebelo. Células de Purkinje',
+            'Médula espinal',
+            'Médula espinal. Conducto epidimario',
+            'Meninges',
+            'Ganglio raquídeo',
+            'Ganglio raquídeo. Nitrato uranio de Cajal',
+            'Nervio periférico. Corte transversal',
+            'Nervio periférico. Corte longitudinal'
+        ],
+        'Sanguíneo': [
+            'Frotis de sangre humana. Neutrófilos',
+            'Frotis de sangre humana. Eosinófilo',
+            'Frotis de sangre humana. Basófilo, eosinófilo',
+            'Frotis de sangre humana. Monocito',
+            'Frotis de sangre humana. Linfocitos',
+            'Reticulocitos',
+            'Frotis de médula ósea',
+            'Hígado fetal. Hematopoyesis extramedular',
+            'Sangre de batracio'
+        ]
+    },
+    'lista_sistemas': {
+        'Circulatorio': [
+            'Corazón',
+            'Pericardio. Miocardio',
+            'Endocarpio',
+            'Arteria elástica',
+            'Aorta',
+            'Arteria muscular',
+            'Arteria muscular. Orceína',
+            'Vena',
+            'Capilares sinusoides en hígado'
+        ],
+        'Endocrino': [
+            'Hipófisis',
+            'Porción distal de hipófisis',
+            'Pars Nerviosa',
+            'Pars Intermedia',
+            'Tiroides',
+            'Tiroides. Van Gieson',
+            'Paratiroides',
+            'Paratiroides. Gran Aumento',
+            'Glándula suprarrenal',
+            'Glándula suprarrenal. Van Gieson'
+        ],
+        'Linfoide': [
+            'Ganglio linfático',
+            'Timo',
+            'Timo. Gran Aumento',
+            'Bazo',
+            'Bazo. Van Gieson',
+            'Amígdala palatina',
+            'Amígdala faríngea',
+            'Apéndice cecal. Folículos linfoides',
+            'Apéndice cecal. Van Gieson'
+        ],
+        'Tegumentario': [
+            'Piel delgada',
+            'Piel delgada. Gran Aumento',
+            'Piel delgada. Mallory',
+            'Piel gruesa',
+            'Piel gruesa. Van Gieson',
+            'Piel gruesa. Melanocitos',
+            'Piel cabelluda. Glándulas sebáceas',
+            'Piel cabelluda. Glándulas sudoríparas',
+            'Folículo piloso',
+            'Uña. Corte longitudinal. Eponiquio'
+        ]
+    },
+    'lista_aparatos': {
+        'Respiratorio': [
+            'Fosas nasales',
+            'Epiglotis',
+            'Epiglotis. Mallory',
+            'Tráquea',
+            'Tráquea. Mallory',
+            'Bronquio',
+            'Pulmón',
+            'Pulmón. Mallory'
+        ],
+        'Digestivo': [
+            'Labio',
+            'Pared lateral de boca',
+            'Encía',
+            'Lengua',
+            'Lengua. Van Gieson',
+            'Diente descalcificado',
+            'Odontogénesis',
+            'Glándula parótida',
+            'Glándula submaxilar',
+            'Glándula sublingual'
+        ],
+        'Urinario': [
+            'Riñón',
+            'Riñón. Médula',
+            'Riñón. Corpúsculo renal',
+            'Pelvis renal',
+            'Pelvis renal, Mallory',
+            'Uréter',
+            'Uréter. Mallory',
+            'Vejiga',
+            'Vejiga. Mallory'
+        ],
+        'Reproductor Femenino': [
+            'Ovario',
+            'Ovario. Folículo secundario',
+            'Cuerpo albicans',
+            'Trompra uterina',
+            'Endometrio proliferativo',
+            'Endometrio secretor temprano',
+            'Cuello uterino. Zona de transición',
+            'Endocervix',
+            'Vagina',
+            'Placenta'
+        ],
+        'Reproductor Masculino': [
+            'Testículo',
+            'Testículo. Células de Leydig',
+            'Epidídimo',
+            'Epidídimo. Mallory',
+            'Conducto deferente',
+            'Conducto deferente. Van Gieson',
+            'Vesícula seminal',
+            'Próstata',
+            'Pene',
+            'Pene. Cuerpo esponjoso'
+        ]
+    },
+    'lista_sentidos_especiales': {
+        'Ojo': [
+            'Ojo fetal',
+            'Córnea',
+            'Cuerpo ciliar',
+            'Retina. Coroides. Esclerótica',
+            'Cristalino'
+        ],
+        'Oido': [
+            'Órgano de Corti',
+            'Pabellón de oreja'
+        ]
+    }
 }
 
 def home(request):
