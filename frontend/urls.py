c86b60966e0fd378c8b1c0ed940229e05e83ead7
Galo Pocoma <gpocoma@msn.com>
2022-08-01 12:29:40 -0400

    Se agregan imagenes de forma automatica desde bucle for (Fix #7)

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, mainmenu
+from .views import home, mainmenu, show
 
 urlpatterns = [
     path('', home, name='home'),
-    path('menu/',mainmenu, name='mainmenu')
+    path('menu/',mainmenu, name='mainmenu'),
+    path('show/<pk>', show, name='show')
 ]
--- frontend/views.py
+++ frontend/views.py
@@ -1,236 +1,224 @@
+from PIL import Image
+from base64 import b64encode
 from django.shortcuts import render
 
 # Create your views here.
 DIC = {
     'prologo': '''
-        En pocas oportunidades se tienen satisfacciones como la que ahora se me
-        presenta para resaltar un precioso material interactivo la del
-        aprendizaje de la histología, elaborado por el Dr. Marco Aliaga, un
-        médico joven, que de esta manera muestra sus potencialidades para la
-        academia y ciencia, y de quien me enorgullezco haber sido su
-        colaborador en este emprendimiento.
-        &#10 
-        Es un hito para la historia de la Facultad porque el material se ha
-        producido en la Cátedra, con fotografías obtenidas de las laminillas
-        creadas por nuestras histotecnólogas y que tiene el objetivo de
-        facilitar el aprendizaje y la enseñanza de estudiantes y
-        académicos.
-        &#10
-        Lo interesante, es que con este medio interactivo pretendemos promover
-        el aprendizaje independiente y el uso de las nuevas tecnologías en
-        sustitución del microscopio, al que le quedan reservadas otras
-        funciones. De esta manera, el estudiante podrá aprender histología sin
-        necesidad de contar con un microscopio facilitando las tareas de los
-        docentes.
-        &#10
+        En pocas oportunidades se tienen satisfacciones como la que ahora se me presenta para resaltar un precioso material interactivo la del aprendizaje de la histología, elaborado por el Dr. Marco Aliaga, un médico joven, que de esta manera muestra sus potencialidades para la academia y ciencia, y de quien me enorgullezco haber sido su colaborador en este emprendimiento.
+        
+        Es un hito para la historia de la Facultad porque el material se ha producido en la Cátedra, con fotografías obtenidas de las laminillas creadas por nuestras histotecnólogas y que tiene el objetivo de facilitar el aprendizaje y la enseñanza de estudiantes y académicos.
+        
+        Lo interesante, es que con este medio interactivo pretendemos promover el aprendizaje independiente y el uso de las nuevas tecnologías en sustitución del microscopio, al que le quedan reservadas otras funciones. De esta manera, el estudiante podrá aprender histología sin necesidad de contar con un microscopio facilitando las tareas de los docentes.
+        
         Guido Zambrana Avila
         DECANO
     ''',
     'lista_epitelio': [
-        'Epitelio plano simple',
-        'Epitelio cúbico simple',
-        'Epitelio cilíndrico simple',
-        'Epitelio cilíndrico simple con esterocilios',
-        'Epitelio cilíndrico simple con microvellosidades',
-        'Epitelio plano estratificado no queratinizado',
-        'Epitelio plano estratificado queratinizado',
-        'Epitelio cilíndrico pseudoestratificado con esterocilios',
-        'Epitelio cilíndrico pseudoestratificado con cilios',
-        'Epitelio de transición'
+        ('Epitelio plano simple',1),
+        ('Epitelio cúbico simple',2),
+        ('Epitelio cilíndrico simple',3),
+        ('Epitelio cilíndrico simple con esterocilios',4),
+        ('Epitelio cilíndrico simple con microvellosidades',5),
+        ('Epitelio plano estratificado no queratinizado',6),
+        ('Epitelio plano estratificado queratinizado',7),
+        ('Epitelio cilíndrico pseudoestratificado con esterocilios',8),
+        ('Epitelio cilíndrico pseudoestratificado con cilios',9),
+        ('Epitelio de transición',10)
     ],
     'lista_glandulas': [
-        'Células caliciformes',
-        'Glándulas tubulares simples',
-        'Glándulas acinar compuesta',
-        'Glándulas sudoríparas',
-        'Glándulas sebáceas',
-        'Ácinos serosos',
-        'Glándulas mixtas',
-        'Glándulas alveolares'
+        ('Células caliciformes',11),
+        ('Glándulas tubulares simples',12),
+        ('Glándulas acinar compuesta',13),
+        ('Glándulas sudoríparas',14),
+        ('Glándulas sebáceas',15),
+        ('Ácinos serosos',16),
+        ('Glándulas mixtas',17),
+        ('Glándulas alveolares',18)
     ],
     'lista_tejidos': {
         'Conectivo': [
-            'Fibras de colágeno densas y laxas',
-            'Tejido conectivo denso regular',
-            'Tejido reticular',
-            'Tejido conecto elástico',
-            'Plasmocitos',
-            'Histocitos',
-            'Mesénquima',
-            'Tejido mucoso',
-            'Tejido adiposo blanco',
-            'Tejido adiposo pardo',
+            ('Fibras de colágeno densas y laxas',19),
+            ('Tejido conectivo denso regular',20),
+            ('Tejido reticular',21),
+            ('Tejido conecto elástico',22),
+            ('Plasmocitos',23),
+            ('Histocitos',24),
+            ('Mesénquima',25),
+            ('Tejido mucoso',26),
+            ('Tejido adiposo blanco',27),
+            ('Tejido adiposo pardo',28),
         ],
         'Cartilaginoso': [
-            'Cartilago hialino',
-            'Cartilago elástico',
-            'Cartilago elástico, Orceína',
-            'Fibrocartílago, Disco Intervertebral',
-            'Fibrocartílago, Menisco'
+            ('Cartilago hialino',29),
+            ('Cartilago elástico',30),
+            ('Cartilago elástico, Orceína',31),
+            ('Fibrocartílago, Disco Intervertebral',32),
+            ('Fibrocartílago, Menisco',33),
         ],
         'Óseo': [
-            'Hueso desgastado, Corte transversal',
-            'Hueso desgastado, Corte longitudinal',
-            'Hueso compacto descalificado',
-            'Hueso compacto trabecular',
-            'Osteoclastos',
-            'Osificación en base membranosa, Mascarilla fetal',
-            'Osificación endocondral, Mano fetal',
-            'Articulación de codo'
+            ('Hueso desgastado, Corte transversal',34),
+            ('Hueso desgastado, Corte longitudinal',35),
+            ('Hueso compacto descalificado',36),
+            ('Hueso compacto trabecular',37),
+            ('Osteoclastos',38),
+            ('Osificación en base membranosa, Mascarilla fetal',39),
+            ('Osificación endocondral, Mano fetal',40),
+            ('Articulación de codo',41),
         ],
         'Muscular': [
-            'Músculo estriado esquelético. Corte longitudinal',
-            'Músculo estriado esquelético. Corte transversal',
-            'Músculo estriado esquelético. Hematoxilina férrica',
-            'Músculo estriado esquelético. Hematoxilina fosfotúngstica',
-            'Músculo cardiaco. Corte longitudinal',
-            'Músculo cardiaco. Corte transversal',
-            'Músculo cardiaco. Hematoxilina fosfotúngstica',
-            'Músculo liso. Intestino delgado',
-            'Músculo liso. Miometrio'
+            ('Músculo estriado esquelético. Corte longitudinal',42),
+            ('Músculo estriado esquelético. Corte transversal',43),
+            ('Músculo estriado esquelético. Hematoxilina férrica',44),
+            ('Músculo estriado esquelético. Hematoxilina fosfotúngstica',45),
+            ('Músculo cardiaco. Corte longitudinal',46),
+            ('Músculo cardiaco. Corte transversal',47),
+            ('Músculo cardiaco. Hematoxilina fosfotúngstica',48),
+            ('Músculo liso. Intestino delgado',49),
+            ('Músculo liso. Miometrio',50),
         ],
         'Nervioso': [
-            'Cerebro',
-            'Cerebelo',
-            'Cerebelo. Células de Purkinje',
-            'Médula espinal',
-            'Médula espinal. Conducto epidimario',
-            'Meninges',
-            'Ganglio raquídeo',
-            'Ganglio raquídeo. Nitrato uranio de Cajal',
-            'Nervio periférico. Corte transversal',
-            'Nervio periférico. Corte longitudinal'
+            ('Cerebro',51),
+            ('Cerebelo',52),
+            ('Cerebelo. Células de Purkinje',53),
+            ('Médula espinal',54),
+            ('Médula espinal. Conducto epidimario',55),
+            ('Meninges',56),
+            ('Ganglio raquídeo',57),
+            ('Ganglio raquídeo. Nitrato uranio de Cajal',58),
+            ('Nervio periférico. Corte transversal',59),
+            ('Nervio periférico. Corte longitudinal',60),
         ],
         'Sanguíneo': [
-            'Frotis de sangre humana. Neutrófilos',
-            'Frotis de sangre humana. Eosinófilo',
-            'Frotis de sangre humana. Basófilo, eosinófilo',
-            'Frotis de sangre humana. Monocito',
-            'Frotis de sangre humana. Linfocitos',
-            'Reticulocitos',
-            'Frotis de médula ósea',
-            'Hígado fetal. Hematopoyesis extramedular',
-            'Sangre de batracio'
+            ('Frotis de sangre humana. Neutrófilos',61),
+            ('Frotis de sangre humana. Eosinófilo',62),
+            ('Frotis de sangre humana. Basófilo, eosinófilo',63),
+            ('Frotis de sangre humana. Monocito',64),
+            ('Frotis de sangre humana. Linfocitos',65),
+            ('Reticulocitos',66),
+            ('Frotis de médula ósea',67),
+            ('Hígado fetal. Hematopoyesis extramedular',68),
+            ('Sangre de batracio',69),
         ]
     },
     'lista_sistemas': {
         'Circulatorio': [
-            'Corazón',
-            'Pericardio. Miocardio',
-            'Endocarpio',
-            'Arteria elástica',
-            'Aorta',
-            'Arteria muscular',
-            'Arteria muscular. Orceína',
-            'Vena',
-            'Capilares sinusoides en hígado'
+            ('Corazón',70),
+            ('Pericardio. Miocardio',71),
+            ('Endocarpio',72),
+            ('Arteria elástica',73),
+            ('Aorta',74),
+            ('Arteria muscular',75),
+            ('Arteria muscular. Orceína',76),
+            ('Vena',77),
+            ('Capilares sinusoides en hígado',78),
         ],
         'Endocrino': [
-            'Hipófisis',
-            'Porción distal de hipófisis',
-            'Pars Nerviosa',
-            'Pars Intermedia',
-            'Tiroides',
-            'Tiroides. Van Gieson',
-            'Paratiroides',
-            'Paratiroides. Gran Aumento',
-            'Glándula suprarrenal',
-            'Glándula suprarrenal. Van Gieson'
+            ('Hipófisis',79),
+            ('Porción distal de hipófisis',80),
+            ('Pars Nerviosa',81),
+            ('Pars Intermedia',82),
+            ('Tiroides',83),
+            ('Tiroides. Van Gieson',84),
+            ('Paratiroides',85),
+            ('Paratiroides. Gran Aumento',86),
+            ('Glándula suprarrenal',87),
+            ('Glándula suprarrenal. Van Gieson',88),
         ],
         'Linfoide': [
-            'Ganglio linfático',
-            'Timo',
-            'Timo. Gran Aumento',
-            'Bazo',
-            'Bazo. Van Gieson',
-            'Amígdala palatina',
-            'Amígdala faríngea',
-            'Apéndice cecal. Folículos linfoides',
-            'Apéndice cecal. Van Gieson'
+            ('Ganglio linfático',89),
+            ('Timo',90),
+            ('Timo. Gran Aumento',91),
+            ('Bazo',92),
+            ('Bazo. Van Gieson',93),
+            ('Amígdala palatina',94),
+            ('Amígdala faríngea',95),
+            ('Apéndice cecal. Folículos linfoides',96),
+            ('Apéndice cecal. Van Gieson',97),
         ],
         'Tegumentario': [
-            'Piel delgada',
-            'Piel delgada. Gran Aumento',
-            'Piel delgada. Mallory',
-            'Piel gruesa',
-            'Piel gruesa. Van Gieson',
-            'Piel gruesa. Melanocitos',
-            'Piel cabelluda. Glándulas sebáceas',
-            'Piel cabelluda. Glándulas sudoríparas',
-            'Folículo piloso',
-            'Uña. Corte longitudinal. Eponiquio'
+            ('Piel delgada',98),
+            ('Piel delgada. Gran Aumento',99),
+            ('Piel delgada. Mallory',100),
+            ('Piel gruesa',101),
+            ('Piel gruesa. Van Gieson',102),
+            ('Piel gruesa. Melanocitos',103),
+            ('Piel cabelluda. Glándulas sebáceas',104),
+            ('Piel cabelluda. Glándulas sudoríparas',105),
+            ('Folículo piloso',106),
+            ('Uña. Corte longitudinal. Eponiquio',107),
         ]
     },
     'lista_aparatos': {
         'Respiratorio': [
-            'Fosas nasales',
-            'Epiglotis',
-            'Epiglotis. Mallory',
-            'Tráquea',
-            'Tráquea. Mallory',
-            'Bronquio',
-            'Pulmón',
-            'Pulmón. Mallory'
+            ('Fosas nasales',108),
+            ('Epiglotis',109),
+            ('Epiglotis. Mallory',110),
+            ('Tráquea',111),
+            ('Tráquea. Mallory',112),
+            ('Bronquio',113),
+            ('Pulmón',114),
+            ('Pulmón. Mallory',115),
         ],
         'Digestivo': [
-            'Labio',
-            'Pared lateral de boca',
-            'Encía',
-            'Lengua',
-            'Lengua. Van Gieson',
-            'Diente descalcificado',
-            'Odontogénesis',
-            'Glándula parótida',
-            'Glándula submaxilar',
-            'Glándula sublingual'
+            ('Labio',116),
+            ('Pared lateral de boca',117),
+            ('Encía',118),
+            ('Lengua',119),
+            ('Lengua. Van Gieson',119),
+            ('Diente descalcificado',120),
+            ('Odontogénesis',121),
+            ('Glándula parótida',122),
+            ('Glándula submaxilar',123),
+            ('Glándula sublingual',124),
         ],
         'Urinario': [
-            'Riñón',
-            'Riñón. Médula',
-            'Riñón. Corpúsculo renal',
-            'Pelvis renal',
-            'Pelvis renal, Mallory',
-            'Uréter',
-            'Uréter. Mallory',
-            'Vejiga',
-            'Vejiga. Mallory'
+            ('Riñón',125),
+            ('Riñón. Médula',126),
+            ('Riñón. Corpúsculo renal',127),
+            ('Pelvis renal',128),
+            ('Pelvis renal, Mallory',128),
+            ('Uréter',129),
+            ('Uréter. Mallory',130),
+            ('Vejiga',131),
+            ('Vejiga. Mallory',132),
         ],
         'Reproductor Femenino': [
-            'Ovario',
-            'Ovario. Folículo secundario',
-            'Cuerpo albicans',
-            'Trompra uterina',
-            'Endometrio proliferativo',
-            'Endometrio secretor temprano',
-            'Cuello uterino. Zona de transición',
-            'Endocervix',
-            'Vagina',
-            'Placenta'
+            ('Ovario',133),
+            ('Ovario. Folículo secundario',134),
+            ('Cuerpo albicans',135),
+            ('Trompra uterina',136),
+            ('Endometrio proliferativo',137),
+            ('Endometrio secretor temprano',138),
+            ('Cuello uterino. Zona de transición',139),
+            ('Endocervix',140),
+            ('Vagina',141),
+            ('Placenta',142),
         ],
         'Reproductor Masculino': [
-            'Testículo',
-            'Testículo. Células de Leydig',
-            'Epidídimo',
-            'Epidídimo. Mallory',
-            'Conducto deferente',
-            'Conducto deferente. Van Gieson',
-            'Vesícula seminal',
-            'Próstata',
-            'Pene',
-            'Pene. Cuerpo esponjoso'
+            ('Testículo',143),
+            ('Testículo. Células de Leydig',144),
+            ('Epidídimo',145),
+            ('Epidídimo. Mallory',146),
+            ('Conducto deferente',147),
+            ('Conducto deferente. Van Gieson',148),
+            ('Vesícula seminal',149),
+            ('Próstata',150),
+            ('Pene',151),
+            ('Pene. Cuerpo esponjoso',152),
         ]
     },
     'lista_sentidos_especiales': {
         'Ojo': [
-            'Ojo fetal',
-            'Córnea',
-            'Cuerpo ciliar',
-            'Retina. Coroides. Esclerótica',
-            'Cristalino'
+            ('Ojo fetal',153),
+            ('Córnea',154),
+            ('Cuerpo ciliar',155),
+            ('Retina. Coroides. Esclerótica',156),
+            ('Cristalino',157),
         ],
         'Oido': [
-            'Órgano de Corti',
-            'Pabellón de oreja'
+            ('Órgano de Corti',158),
+            ('Pabellón de oreja',159),
         ]
     }
 }
@@ -242,3 +230,13 @@ def home(request):
 def mainmenu(request):
     context = DIC
     return render(request,'menu.html',context=context)
+
+def show(request, pk):
+    context = DIC
+    context['pk'] = pk
+    for i in range(1,160):
+        if int(pk) == i:
+            context['ruta_imagen'] = f'img/{i}.jpg'
+            break
+    
+    return render(request,'menu.html', context=context)
