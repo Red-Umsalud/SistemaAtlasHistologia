b251461ed41720c1744ac9daf80d10baa33aafa7
Galo Pocoma <gpocoma@msn.com>
2022-08-15 15:33:17 -0400

    Base de datos modificada (Fix #13)

---
+++ db/data.sql
@@ -0,0 +1,265 @@
+-- Active: 1660582233505@@127.0.0.1@3306
+INSERT INTO elemento(nombre)
+VALUES
+('Prologo'),
+('Epitelio'),
+('Glándulas'),
+('Tejidos'),
+('Sistemas'),
+('Aparatos'),
+('Sentidos Especiales'),
+('Índice Alfabético'),
+('Evaluaciones'),
+('Bibliografía');
+
+INSERT INTO elemento(nombre,ele_id_elemento)
+VALUES
+    ('Conectivo', 4),
+    ('Cartilaginoso', 4),
+    ('Óseo', 4),
+    ('Muscular', 4),
+    ('Nervioso', 4),
+    ('Sanguíneo', 4),
+    ('Circulatorio', 5),
+    ('Endocrino', 5),
+    ('Linfoide', 5),
+    ('Tegumentario', 5),
+    ('Respiratorio', 6),
+    ('Digestivo', 6),
+    ('Urinario', 6),
+    ('Reproductor Femenino', 6),
+    ('Reproducto Masculino', 6),
+    ('Ojo', 7),
+    ('Oído', 7);
+
+INSERT INTO elemento(nombre,ele_id_elemento) VALUES
+    ('Epitelio plano simple', 2),
+    ('Epitelio cúbico simple', 2),
+    ('Epitelio cilíndrico simple', 2),
+    ('Epitelio cilíndrico simple con esterocilios', 2),
+    ('Epitelio cilíndrico simple con microvellosidades', 2),
+    ('Epitelio plano estratificado no queratinizado', 2),
+    ('Epitelio plano estratificado queratinizado', 2),
+    ('Epitelio cilíndrico pseudoestratificado con esterocilios', 2),
+    ('Epitelio cilíndrico pseudoestratificado con cilios', 2),
+    ('Epitelio de transición', 2);
+
+INSERT INTO elemento(nombre, ele_id_elemento) VALUES
+    ('Células caliciformes', 3),
+    ('Glándulas tubulares simples', 3),
+    ('Glándula acinar compuesta', 3),
+    ('Glándulas sudoriparas', 3),
+    ('Glándulas sebáceas', 3),
+    ('Ácinos serosos', 3),
+    ('Glándulas mixtas', 3),
+    ('Glándulas alveolares', 3);
+
+INSERT INTO elemento(nombre, ele_id_elemento) VALUES
+    ('Fibras de colágeno densas y laxas', 11),
+    ('Tejido conectivo denso regular', 11),
+    ('Tejido reticular', 11),
+    ('Tejido conectivo elástico', 11),
+    ('Plasmocitos', 11),
+    ('Histiocitos', 11),
+    ('Mesénquima', 11),
+    ('Tejido mucoso', 11),
+    ('Tejido adiposo blanco', 11),
+    ('Tejido adiposo pardo', 11);
+
+INSERT INTO elemento(nombre, ele_id_elemento) VALUES
+    ('Cartílago hialino', 12),
+    ('Cartílago elástico', 12),
+    ('Cartílago elástico. Orceína', 12),
+    ('Fibrocartílago. Disco Intervertebral', 12),
+    ('Fibrocartílago. Menisco', 12);
+
+INSERT INTO elemento(nombre,ele_id_elemento) VALUES
+    ('Hueso desgastado. Corte transversal', 13),
+    ('Hueso desgastado. Corte longitudinal', 13),
+    ('Hueso compacto descalcificado', 13),
+    ('Hueso compacto trabecular', 13),
+    ('Osteoclástos', 13),
+    ('Osificación en base membranosa. Mascarilla fetal', 13),
+    ('Osificación endocondral. Mano fetal', 13),
+    ('Articulación de codo', 13);
+
+INSERT INTO elemento(nombre, ele_id_elemento) VALUES
+    ('Músculo estriado esquelético. Corte longitudinal', 14),
+    ('Músculo estriado esquelético. Corte transversal', 14),
+    ('Músculo estriado esquelético. Hematoxilina férrica', 14),
+    ('Músculo estriado esquelético. Hematoxilina fosfotúngstica',14),
+    ('Músculo cardiaco. Corte longitudinal', 14),
+    ('Músculo cardiaco. Corte transversal', 14),
+    ('Músculo cardiaco. Hematoxilina fosfotúngstica', 14),
+    ('Músculo liso. Intestino delgado', 14),
+    ('Músculo liso. Miometrio', 14);
+
+INSERT INTO elemento(nombre, ele_id_elemento) VALUES
+    ('Cerebro', 15),
+    ('Cerebelo', 15),
+    ('Cerebelo. Células de Purkinje', 15),
+    ('Médula espinal', 15),
+    ('Médula espinal. Conducto ependimario', 15),
+    ('Meninges', 15),
+    ('Ganglio raquídeo', 15),
+    ('Ganglio raquídeo. Nitrato uranio de Cajal', 15),
+    ('Nervio periférico. Corte transversal', 15),
+    ('Nervio periférico. Corte longitudinal', 15),
+    ('Nervio periférico. Células de Schwann', 15),
+    ('Neuroglia. Astrocitos fibrosos', 15),
+    ('Neuroglia. Astrocito protoplasmático', 15),
+    ('Plexo mientérico de Auerbach', 15);
+
+INSERT INTO elemento(nombre, ele_id_elemento) VALUES
+    ('Frotis de sangre humana. Neutrófilos', 16),
+    ('Frotis de sangre humana. Eosinófilo', 16),
+    ('Frotis de sangre humana. Basófilo, eosinófilo', 16),
+    ('Frotis de sangre humana. Monocito', 16),
+    ('Frotis de sangre humana. Linfocitos', 16),
+    ('Reticulocitos', 16),
+    ('Frotis de médula ósea', 16),
+    ('Hígado fetal. Hematopoyesis extramedular', 16),
+    ('Sangre de Batracio', 16);
+
+INSERT INTO elemento(nombre, ele_id_elemento) VALUES
+    ('Corazón', 17),
+    ('Pericardio. Miocardio', 17),
+    ('Endocardio', 17),
+    ('Arteria elástica', 17),
+    ('Aorta', 17),
+    ('Arteria muscular', 17),
+    ('Arteria muscular. Orceína', 17),
+    ('Vena', 17),
+    ('Capilares sinusoides en hígado', 17);
+
+INSERT INTO elemento(nombre, ele_id_elemento) VALUES
+    ('Hipófisis', 18),
+    ('Porción distal de hipófisis', 18),
+    ('Pars Nerviosa', 18),
+    ('Pars Intermedia', 18),
+    ('Tiroides', 18),
+    ('Tiroides. Van Gieson', 18),
+    ('Paratiroides', 18),
+    ('Paratiroides. Gran aumento', 18),
+    ('Glándula suprarrenal', 18),
+    ('Glándula suprarrenal. Van Gieson', 18),
+    ('Corteza suprarrenal', 18),
+    ('Glándula pineal', 18),
+    ('Glándula pineal. Mallory', 18);
+
+INSERT INTO elemento(nombre, ele_id_elemento) VALUES
+    ('Ganglio linfático', 19),
+    ('Timo', 19),
+    ('Timo. Gran aumento', 19),
+    ('Bazo', 19),
+    ('Bazo. Van Gieson', 19),
+    ('Amígdala palatina', 19),
+    ('Amígdala faríngea', 19),
+    ('Apéndice cecal. Folículos linfoides', 19),
+    ('Apéndice cecal. Van Gieson', 19);
+
+INSERT INTO elemento(nombre, ele_id_elemento) VALUES
+    ('Piel delgada', 20),
+    ('Piel delgada. Gran aumento', 20),
+    ('Piel delgada. Mallory', 20),
+    ('Piel gruesa', 20),
+    ('Piel gruesa. Van Gieson', 20),
+    ('Piel gruesa. Melanocitos', 20),
+    ('Piel cabelluda. Glándulas sebáceas', 20),
+    ('Piel cabelluda. Glándulas sudoríparas', 20),
+    ('Folículo piloso', 20),
+    ('Uña. Corte longitudinal. Eponiquio', 20),
+    ('Uña. Corte longitudinal. Hiponiquio', 20),
+    ('Uña. Corte transversal', 20);
+ 
+INSERT INTO elemento(nombre, ele_id_elemento) VALUES
+    ('Fosas nasales', 21),
+    ('Epiglotis', 21),
+    ('Epiglotis. Mallory', 21),
+    ('Tráquea', 21),
+    ('Tráquea. Mallory', 21),
+    ('Bronquio', 21),
+    ('Pulmón', 21),
+    ('Pulmón. Mallory', 21);
+
+INSERT INTO elemento(nombre, ele_id_elemento) VALUES
+    ('Labio', 22),
+    ('Pared lateral de boca', 22),
+    ('Encía', 22),
+    ('Lengua', 22),
+    ('Lengua. Van Gieson', 22),
+    ('Diente descalcificado', 22),
+    ('Odontogénesis', 22),
+    ('Glándula parótida', 22),
+    ('Glándula submaxilar', 22),
+    ('Glándula sublingual', 22),
+    ('Faringe', 22),
+    ('Esófago', 22),
+    ('Esófago. Mallory', 22),
+    ('Estómago', 22),
+    ('Estómago. Glándulas fúndicas', 22),
+    ('Duodeno', 22),
+    ('Intestino delgado', 22),
+    ('Intestino delgado. Vellosidades', 22),
+    ('Intestino delgado. Plexo de Auerbach', 22),
+    ('Intestino grueso', 22),
+    ('Apéndice cecal', 22),
+    ('Hígado humano', 22),
+    ('Hígado humano. Van Gieson', 22),
+    ('Hígado humano. Espacio porta', 22),
+    ('Hígado de cerdo', 22),
+    ('Hígado de cerdo. Mallory', 22),
+    ('Vesícula biliar', 22),
+    ('Vesícula biliar. Van Gieson', 22),
+    ('Páncreas', 22),
+    ('Páncreas. Van Gieson', 22);
+
+INSERT INTO elemento(nombre,ele_id_elemento) VALUES
+    ('Riñón', 23),
+    ('Riñón. Médula', 23),
+    ('Riñón. Corpúsculo renal', 23),
+    ('Pelvis renal', 23),
+    ('Pelvis renal. Mallory', 23),
+    ('Uréter', 23),
+    ('Uréter. Mallory', 23),
+    ('Vejiga', 23),
+    ('Vejiga. Mallory', 23);
+
+INSERT INTO elemento(nombre, ele_id_elemento) VALUES
+    ('Ovario', 24),   
+    ('Ovario. Folículo secundario', 24),
+    ('Cuerpo albicans', 24),
+    ('Trompta uterina', 24),
+    ('Endometrio proliferativo', 24),
+    ('Endometrio secretor temprano', 24),
+    ('Cuello uterino. Zona de transición', 24),
+    ('Endocervix', 24),
+    ('Vagina', 24),
+    ('Placenta', 24),
+    ('Cordón umbilical', 24),
+    ('Glándula mamaria', 24);
+
+INSERT INTO elemento(nombre, ele_id_elemento) VALUES
+    ('Testículo', 25),
+    ('Testículo. Células de Leydig', 25),
+    ('Epididimo', 25),
+    ('Epididimo. Mallory', 25),
+    ('Conducto deferente', 25),
+    ('Conducto deferente. Van Gieson', 25),
+    ('Vesícula seminal', 25),
+    ('Próstata', 25),
+    ('Pene', 25),
+    ('Pene. Cuerpo esponjoso', 25),
+    ('Pene. Cuerpo cavernoso', 25);
+
+INSERT INTO elemento(nombre, ele_id_elemento) VALUES
+    ('Ojo fetal', 26),
+    ('Córnea', 26),
+    ('Cuerpo ciliar', 26),
+    ('Retina. Coroides. Esclerótica', 26),
+    ('Cristalino', 26);
+
+INSERT INTO elemento(nombre, ele_id_elemento) VALUES
+    ('Órgano de Corti', 27),
+    ('Pabellón de oreja', 27)
+
--- db/db.sql
+++ db/db.sql
@@ -1,14 +1,15 @@
+-- Active: 1660585423839@@127.0.0.1@5432@db_histologia
 /*==============================================================*/
 /* DBMS name:      PostgreSQL 9.x                               */
-/* Created on:     15/8/2022 12:26:17                           */
+/* Created on:     15/8/2022 13:37:28                           */
 /*==============================================================*/
 
+
+drop index CONTIENEOTRO_FK;
 
 drop index ELEMENTO_PK;
 
 drop table ELEMENTO;
-
-drop index TIENE_FICHA_FK;
 
 drop index TIENE_FK;
 
@@ -22,17 +23,12 @@ drop index FOTOGRAFIA_PK;
 
 drop table FOTOGRAFIA;
 
-drop index CONTIENE_FK;
-
-drop index SUBELEMENTO_PK;
-
-drop table SUBELEMENTO;
-
 /*==============================================================*/
 /* Table: ELEMENTO                                              */
 /*==============================================================*/
 create table ELEMENTO (
    ID_ELEMENTO          SERIAL               not null,
+   ELE_ID_ELEMENTO      INT4                 null,
    NOMBRE               VARCHAR(256)         not null,
    constraint PK_ELEMENTO primary key (ID_ELEMENTO)
 );
@@ -45,12 +41,18 @@ ID_ELEMENTO
 );
 
 /*==============================================================*/
+/* Index: CONTIENEOTRO_FK                                       */
+/*==============================================================*/
+create  index CONTIENEOTRO_FK on ELEMENTO (
+ELE_ID_ELEMENTO
+);
+
+/*==============================================================*/
 /* Table: FICHA                                                 */
 /*==============================================================*/
 create table FICHA (
    ID_FICHA             SERIAL               not null,
    ID_ELEMENTO          INT4                 null,
-   ID_SUBELEMENTO       INT4                 null,
    NUMERO               INT4                 not null,
    TITULO               VARCHAR(1024)        not null,
    CONTENIDO            TEXT                 not null,
@@ -69,13 +71,6 @@ ID_FICHA
 /*==============================================================*/
 create  index TIENE_FK on FICHA (
 ID_ELEMENTO
-);
-
-/*==============================================================*/
-/* Index: TIENE_FICHA_FK                                        */
-/*==============================================================*/
-create  index TIENE_FICHA_FK on FICHA (
-ID_SUBELEMENTO
 );
 
 /*==============================================================*/
@@ -103,47 +98,18 @@ create  index TIENE_FOTO_FK on FOTOGRAFIA (
 ID_FICHA
 );
 
-/*==============================================================*/
-/* Table: SUBELEMENTO                                           */
-/*==============================================================*/
-create table SUBELEMENTO (
-   ID_SUBELEMENTO       SERIAL               not null,
-   ID_ELEMENTO          INT4                 not null,
-   NOMBRE               VARCHAR(256)         not null,
-   constraint PK_SUBELEMENTO primary key (ID_SUBELEMENTO)
-);
-
-/*==============================================================*/
-/* Index: SUBELEMENTO_PK                                        */
-/*==============================================================*/
-create unique index SUBELEMENTO_PK on SUBELEMENTO (
-ID_SUBELEMENTO
-);
-
-/*==============================================================*/
-/* Index: CONTIENE_FK                                           */
-/*==============================================================*/
-create  index CONTIENE_FK on SUBELEMENTO (
-ID_ELEMENTO
-);
+alter table ELEMENTO
+   add constraint FK_ELEMENTO_CONTIENEO_ELEMENTO foreign key (ELE_ID_ELEMENTO)
+      references ELEMENTO (ID_ELEMENTO)
+      on delete restrict on update restrict;
 
 alter table FICHA
    add constraint FK_FICHA_TIENE_ELEMENTO foreign key (ID_ELEMENTO)
       references ELEMENTO (ID_ELEMENTO)
       on delete restrict on update restrict;
 
-alter table FICHA
-   add constraint FK_FICHA_TIENE_FIC_SUBELEME foreign key (ID_SUBELEMENTO)
-      references SUBELEMENTO (ID_SUBELEMENTO)
-      on delete restrict on update restrict;
-
 alter table FOTOGRAFIA
    add constraint FK_FOTOGRAF_TIENE_FOT_FICHA foreign key (ID_FICHA)
       references FICHA (ID_FICHA)
-      on delete restrict on update restrict;
-
-alter table SUBELEMENTO
-   add constraint FK_SUBELEME_CONTIENE_ELEMENTO foreign key (ID_ELEMENTO)
-      references ELEMENTO (ID_ELEMENTO)
       on delete restrict on update restrict;
 
